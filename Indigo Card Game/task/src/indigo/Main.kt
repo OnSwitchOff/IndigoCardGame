2f8213a494346c8cb7eccf1bb6e2959879054017
Viktor Kassov <kassov@mail.ua>
2022-01-11 11:30:20 +0200

    1/5+

--- Indigo Card Game/task/src/indigo/Main.kt
+++ Indigo Card Game/task/src/indigo/Main.kt
@@ -1,5 +1,52 @@
 package indigo
 
 fun main() {
-    println("Hello World!")
+    println(CardRank)
+    println(CardSuit)
+    println(Deck())
+}
+
+enum class CardRank(val sign: String) {
+    ACE("A"),
+    TWO("2"),
+    THREE("3"),
+    FOUR("4"),
+    FIVE("5"),
+    SIX("6"),
+    SEVEN("7"),
+    EIGHT("8"),
+    NINE("9"),
+    TEN("10"),
+    JACK("J"),
+    QUEEN("Q"),
+    KING("K");
+    companion object {
+        override fun toString(): String = values().joinToString(" ") { it.sign }
+    }
+}
+
+enum class CardSuit(val sign: Char){
+    DIAMONDS('♦'),
+    HEARTS('♥'),
+    SPADES('♠'),
+    CLUBS('♣');
+    companion object {
+        override fun toString(): String = values().joinToString(" ") { it.sign.toString() }
+    }
+}
+
+class Deck {
+    val cards: MutableList<Card> = mutableListOf()
+    constructor() {
+        for (suit in CardSuit.values()) {
+            for (rank in CardRank.values()){
+                cards.add(Card(rank,suit))
+            }
+        }
+    }
+    override fun toString(): String = cards.joinToString(" ") { it.toString() }
+}
+
+class Card(val rank: CardRank, val suit: CardSuit){
+    override fun toString(): String = this.rank.sign + this.suit.sign
 }
